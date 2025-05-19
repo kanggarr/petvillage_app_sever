7d3e89a7e8303b77141a18b66892d9e713b96740
PondPP <93928501+PondPP@users.noreply.github.com>
2025-05-19 15:04:03 +0700

    edit profile user

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { validateUserPermission } = require("../middleware/userMiddleware");
-const { test, getCurrentUserProfile } = require('../controllers/userController');
+const { test, getCurrentUserProfile, updateUser, updatePassword } = require('../controllers/userController');
 
 /**
  * @route GET /api/user/test
@@ -13,6 +13,7 @@ router.get('/test', validateUserPermission, test);
 // All routes below this middleware will require authentication
 router.use(validateUserPermission);
 router.get('/profile', getCurrentUserProfile);
-// router.put('/profile', updateUserProfile);
+router.put('/update', validateUserPermission, updateUser);
+router.put('/password', validateUserPermission, updatePassword);
 
 module.exports = router;
